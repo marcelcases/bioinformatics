433bc46f86927050e8e0b85117a2e075f8333cf6
Marcel Cases i Freixenet <m@rcel.xyz>
2021-10-14 08:17:30 +0200

    Lab 1 finished, notebook and script formats

--- robinson-foulds/main.py
+++ robinson-foulds/main.py
@@ -1,53 +1,101 @@
 """
-Robinson-Foulds distance between two phylogenetic trees
-MIRI-BSG
-Marcel Cases Freixenet
-Autumn 2021
+# Lab 1: Robinson-Foulds distance
 
-It is defined as (A + B) where A is the number of partitions of data implied by
-the first tree but not the second tree and B is the number of partitions of data
-implied by the second tree but not the first tree.
+Implementation in Python of the quadratic-time algorithm for the Robinson-Foulds distance between two phylogenetic trees.
 
-The partitions are calculated for each tree by removing each branch.
+## About
 
-The number of eligible partitions for each tree is equal to the number of branches in that tree.
+**Course**  
+Bioinformatics and Statistical Genetics (BSG-MIRI)  
+FIB - Universitat Politècnica de Catalunya. BarcelonaTech  
+Autumn 2021  
+
+**Team**  
+* Antoni Casas
+&lt;antoni.casas.munoz@estudiantat.upc.edu&gt;
+* Marcel Cases
+&lt;marcel.cases@estudiantat.upc.edu&gt;
 """
 
-from Bio import Phylo
-from io import StringIO
-import os
+"""
+## The algorithm
 
-FOLDER_TOP = os.path.dirname(os.path.abspath(__file__))
+We use `ete3` as the library for parsing phylogenetic trees from a string.
+We use the Newick representation.
+"""
 
-class  RobinsonFoulds():
-    def __init__(self):
-        super().__init__()
+from ete3 import Tree
 
-class Draw():
-    def __init__(self):
-        super().__init__()
-    
-    def raw(self, tree):
-        print(tree)
-    
-    def ascii(self, tree):
-        Phylo.draw_ascii(tree)
-    
-    def plot(self, tree):
-        tree.ladderize()  # Flip branches so deeper clades are displayed at top
-        Phylo.draw(tree)
-    
-    def dendrogram(self, tree):
-        Phylo.draw_graphviz(tree)
+# Loads a tree structure from a newick string. The returned variable ’t’ is the root node for the tree.
+t = Tree("(((1,2),3),4);" )
+t2 = Tree("(1,(2,(3,4)));" )
 
-if __name__ == "__main__":
-    draw = Draw()
-    rf = RobinsonFoulds()
-    tree1a = Phylo.read(FOLDER_TOP+'/example.xml','phyloxml')
-    tree1b = Phylo.read(StringIO("((A, B), C)"), "newick")
-    tree2 = Phylo.read(StringIO("(A, (B, C), (D, E))"), "newick")
-    # draw.raw(tree1)
-    # draw.ascii(tree1a)
-    # draw.ascii(tree1b)
-    draw.plot(tree1b)
-    # draw.dendrogram(tree1b)
+
+"""
+The input trees that we want to use for calculating the distance with the Robinson-Foulds algorithm are the following two:
+"""
+
+print("t:", t)
+print("t2:", t2)
+
+
+"""
+We build a recursive function that takes each child from the node up to the last in each branch and applies a reverse process in order to identify sets from nodes
+"""
+
+def reverseTraversal(node, resultSet):
+    currentClust=set()
+    for child in node.children:
+        leafClust=reverseTraversal(child,resultSet)
+        currentClust=currentClust.union(leafClust)
+    currentClust=currentClust.union(node.name)
+    resultSet.append(currentClust)
+    return currentClust
+
+
+"""
+Once we have identified all sets in each tree, ...
+"""
+
+sets1=[]
+reverseTraversal(t,sets1)
+print("Sets 1:", sets1)
+
+sets2=[]
+reverseTraversal(t2,sets2)
+print("Sets 2:", sets2)
+
+
+lenSets1=len(sets1)
+lenSets2=len(sets2)
+
+distance=0
+
+for val in sets1:
+    contained=False
+    currentIt=0
+    while not contained and currentIt<len(sets2):
+        auxVal=sets2[currentIt]
+        if (auxVal==val):
+            contained=True
+        currentIt=currentIt+1
+    if (not contained):
+        distance=distance+1
+
+for val in sets2:
+    contained=False
+    currentIt=0
+    while not contained and currentIt<len(sets1):
+        auxVal=sets1[currentIt]
+        if (auxVal==val):
+            contained=True
+        currentIt=currentIt+1
+    if (not contained):
+        distance=distance+1
+
+
+"""
+Finally, the computed distance between ``t`` and ``t2`` is:
+"""
+
+print("R-F distance:", distance)
